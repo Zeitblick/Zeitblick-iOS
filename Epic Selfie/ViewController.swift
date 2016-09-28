4fbc3c67ccd6387709a543323e7f7772abb736ef
bclausdorff <b.clausdorff@posteo.de>
2016-09-29 00:26:42 +0200

    Add imagePicker with source .camera

--- Epic Selfie/ViewController.swift
+++ Epic Selfie/ViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var photoButton: DesignableButton!
+
+    private lazy var imagePicker: UIImagePickerController = {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        return imagePicker
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        photoButton.addTarget(self, action: #selector(takePhoto), for: .touchUpInside)
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // mark: actions
+    func takePhoto() {
+        present(imagePicker, animated: true, completion: nil)
     }
-
 
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        print(#function)
+        dismiss(animated: true, completion: nil)
+    }
+}
