d74d948dadb0b68cf8fec265b6e7a419ea5ee963
bclausdorff <b.clausdorff@posteo.de>
2016-10-03 14:07:00 +0200

    Upload and evaluate image

--- Epic Selfie/ViewController.swift
+++ Epic Selfie/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
 
 class ViewController: UIViewController {
 
@@ -37,43 +39,43 @@ class ViewController: UIViewController {
     }
 
     func submitPhoto() {
+        self.view.showLoading()
         guard let image = self.image ?? nil else {
             print("no image")
             return
         }
 
-        guard let imageBase64 = ImageHelper.base64EncodeImage(image: image) else {
-            print("couldn't encode image")
+        guard let imageBase64 = ImageHelper.prepareForGoogleCloud(image: image) else {
+            print("couldn't prepare image")
             return
         }
 
         createRequest(imageData: imageBase64)
-
     }
 
     func createRequest(imageData: String) {
         // Create our request URL
         let API_KEY = Environment.sharedInstance.googleCloudApiKey
         let url = URL(string: "https://vision.googleapis.com/v1/images:annotate?key=\(API_KEY)")!
-        let request = MutableURLRequest(url: url)
+        var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue(Bundle.main.bundleIdentifier ?? "", forHTTPHeaderField: "X-Ios-Bundle-Identifier")
 
         // Build our API request
-        let jsonRequest: Parameters = [
+        let jsonRequest: [String: Any] = [
             "requests": [
                 "image": [
                     "content": imageData
                 ],
                 "features": [
-//                    [
-//                        "type": "LABEL_DETECTION",
-//                        "maxResults": 10
-//                    ],
+                    [
+                        "type": "LABEL_DETECTION",
+                        "maxResults": 10
+                    ],
                     [
                         "type": "FACE_DETECTION",
-                        "maxResults": 10
+                        "maxResults": 1
                     ]
                 ]
             ]
@@ -85,16 +87,9 @@ class ViewController: UIViewController {
         request.httpBody = try! JSONSerialization.data(withJSONObject: jsonRequest, options: [])
 
         // Run the request on a background thread
-
-        self.view.showLoading()
-
         DispatchQueue.global(qos: .background).async {
-            self.runRequestOnBackgroundThread(request as URLRequest)
+            self.runRequestOnBackgroundThread(request)
         }
-//        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
-
-
-
     }
 
 
@@ -103,15 +98,99 @@ class ViewController: UIViewController {
 
         // run the request
         let task = session.dataTask(with: request, completionHandler: {data, response, error -> Void in
-//            self.analyzeResults(data!)
             print(response)
             print(data)
             DispatchQueue.main.sync {
                 self.view.hideLoading()
+                self.analyzeResults(data!)
             }
         })
         print("fire")
         task.resume()
+    }
+
+    func analyzeResults(_ dataToParse: Data) {
+            // Use SwiftyJSON to parse results
+            let json = JSON(data: dataToParse)
+            let errorObj: JSON = json["error"]
+
+//            self.spinner.stopAnimating()
+//            self.imageView.hidden = true
+//            self.labelResults.hidden = false
+//            self.faceResults.hidden = false
+//            self.faceResults.text = ""
+
+            // Check for errors
+            if (errorObj.dictionaryValue != [:]) {
+//                self.labelResults.text = "Error code \(errorObj["code"]): \(errorObj["message"])"
+                print("Error code \(errorObj["code"]): \(errorObj["message"])")
+            } else {
+                // Parse the response
+                print(json)
+                let responses: JSON = json["responses"][0]
+
+                // Get face annotations
+                let faceAnnotations: JSON = responses["faceAnnotations"]
+                if faceAnnotations != nil {
+                    let emotions: Array<String> = ["joy", "sorrow", "surprise", "anger"]
+
+                    let numPeopleDetected:Int = faceAnnotations.count
+
+//                    self.faceResults.text = "People detected: \(numPeopleDetected)\n\nEmotions detected:\n"
+                    print("People detected: \(numPeopleDetected)\n\nEmotions detected:\n")
+
+                    var emotionTotals: [String: Double] = ["sorrow": 0, "joy": 0, "surprise": 0, "anger": 0]
+                    var emotionLikelihoods: [String: Double] = ["VERY_LIKELY": 0.9, "LIKELY": 0.75, "POSSIBLE": 0.5, "UNLIKELY":0.25, "VERY_UNLIKELY": 0.0]
+
+                    for index in 0..<numPeopleDetected {
+                        let personData:JSON = faceAnnotations[index]
+
+                        // Sum all the detected emotions
+                        for emotion in emotions {
+                            let lookup = emotion + "Likelihood"
+                            let result:String = personData[lookup].stringValue
+                            emotionTotals[emotion]! += emotionLikelihoods[result]!
+                        }
+                    }
+                    // Get emotion likelihood as a % and display in UI
+                    for (emotion, total) in emotionTotals {
+                        let likelihood:Double = total / Double(numPeopleDetected)
+                        let percent: Int = Int(round(likelihood * 100))
+//                        self.faceResults.text! += "\(emotion): \(percent)%\n"
+                        print("\(emotion): \(percent)%\n")
+                    }
+                } else {
+//                    self.faceResults.text = "No faces found"
+                    print("No faces found")
+                }
+
+                // Get label annotations
+                let labelAnnotations: JSON = responses["labelAnnotations"]
+                let numLabels: Int = labelAnnotations.count
+                var labels: Array<String> = []
+                if numLabels > 0 {
+                    var labelResultsText:String = "Labels found: "
+                    for index in 0..<numLabels {
+                        let label = labelAnnotations[index]["description"].stringValue
+                        labels.append(label)
+                    }
+                    for label in labels {
+                        // if it's not the last item add a comma
+                        if labels[labels.count - 1] != label {
+                            labelResultsText += "\(label), "
+                        } else {
+                            labelResultsText += "\(label)"
+                        }
+                    }
+//                    self.labelResults.text = labelResultsText
+                        print(labelResultsText)
+                } else {
+//                    self.labelResults.text = "No labels found"
+                    print("No labels found")
+                }
+            }
+//        }
+
     }
 }
 
